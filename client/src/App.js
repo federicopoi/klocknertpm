f63127abe10162ac135ad89aa91f5eb10db0bdba
Federico Poi <fedepoi@MacBook-Pro-de-Federico.local>
2021-03-02 11:10:50 -0600

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import { connect } from "react-redux";
 import SearchBy from "./search/SearchBy";
 import AdminUsuarios from "./components/admin/AdminUsuarios";
 import MisTarjetasFiltro from "./components/tarjetas/MisTarjetasFiltro";
+import "react-app-polyfill/stable";
 
 class App extends Component {
   componentDidMount() {
