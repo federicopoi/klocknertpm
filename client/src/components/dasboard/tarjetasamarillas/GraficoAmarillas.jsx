1b0c8958b58ccaea20753ba370c0fae0d47041b1
Federico Poi <fedepoi@MacBook-Pro-de-Federico.local>
2021-04-14 19:32:05 -0500

    make it better

--- client/src/components/dasboard/tarjetasamarillas/GraficoAmarillas.jsx
+++ client/src/components/dasboard/tarjetasamarillas/GraficoAmarillas.jsx
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import CanvasJSReact from "../canvasjs.react";
 import moment, { months } from "moment";
 import TableModal from "../tablemodal/TableModal";
-import { Col, Row, Card, CardBody, Table } from "reactstrap";
+import { Col, Row, Card, CardBody, Table, Input, Button } from "reactstrap";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 var CanvasJS = CanvasJSReact.CanvasJS;
 
 export class GraficoAmarillas extends Component {
   constructor() {
     super();
+    this.state = {
+      numberMonths: "12",
+    };
     this.toggleDataSeries = this.toggleDataSeries.bind(this);
   }
   toggleDataSeries(e) {
@@ -20,9 +24,19 @@ export class GraficoAmarillas extends Component {
     this.chart.render();
   }
 
+  onChange = (e) => {
+    e.target.value === "Seleccionar"
+      ? this.setState({
+          [e.target.name]: 12,
+        })
+      : this.setState({
+          [e.target.name]: e.target.value,
+        });
+  };
+
   render() {
     const { tarjetas } = this.props;
-
+    console.log(this.state);
     // Formulas para "Amarillas acumuladas abiertas"
 
     // Filtro todos los meses en el que hay tarjetas abiertas Amarillas
@@ -68,8 +82,12 @@ export class GraficoAmarillas extends Component {
       startDate.add(1, "month");
     }
 
+    const fechastarjetasUnicasRangoCut = fechastarjetasUnicasRango.slice(
+      Math.max(fechastarjetasUnicasRango.length - this.state.numberMonths, 0)
+    );
+
     // Numero total de tarjetas de cada mes (no acumulado)
-    let array = fechastarjetasUnicasRango.sort().map((item, index) => {
+    let array = fechastarjetasUnicasRangoCut.sort().map((item, index) => {
       return tarjetas.filter(
         ({ estado, fecha, color }) =>
           color === "Amarilla" && fecha.slice(0, 7) === item.slice(0, 7)
@@ -82,7 +100,7 @@ export class GraficoAmarillas extends Component {
 
     // Datos para el grafico
     const AmarillasAcumuladasAbiertasData = [
-      fechastarjetasUnicasRango.sort().map((item, index) => {
+      fechastarjetasUnicasRangoCut.sort().map((item, index) => {
         return {
           x: new Date(
             parseInt(item.slice(0, 4)),
@@ -96,14 +114,16 @@ export class GraficoAmarillas extends Component {
     // Formulas para "Amarillas acumuladas cerradas"
 
     // Numero total de tarjetas de cada mes (no acumulado)
-    let arrayCerradas = fechastarjetasUnicasRango.sort().map((item, index) => {
-      return tarjetas.filter(
-        ({ estado, finReparacion, color }) =>
-          color === "Amarilla" &&
-          estado === "Cerrada" &&
-          finReparacion.slice(0, 7) === item.slice(0, 7)
-      ).length;
-    });
+    let arrayCerradas = fechastarjetasUnicasRangoCut
+      .sort()
+      .map((item, index) => {
+        return tarjetas.filter(
+          ({ estado, finReparacion, color }) =>
+            color === "Amarilla" &&
+            estado === "Cerrada" &&
+            finReparacion.slice(0, 7) === item.slice(0, 7)
+        ).length;
+      });
 
     // Acumulado de tarjetas por mes
     const arrTarjetasAmarillasAcumuladasCerradas = arrayCerradas.map(
@@ -113,7 +133,7 @@ export class GraficoAmarillas extends Component {
 
     // Datos para el grafico
     const AmarillasAcumuladasAbiertasDataCerradas = [
-      fechastarjetasUnicasRango.sort().map((item, index) => {
+      fechastarjetasUnicasRangoCut.sort().map((item, index) => {
         return {
           x: new Date(
             parseInt(item.slice(0, 4)),
@@ -127,7 +147,7 @@ export class GraficoAmarillas extends Component {
     // Formulas para "Porcentaje acumuladas cerradas porcentaje"
 
     // Numero total de tarjetas de cada mes (no acumulado)
-    let arrayCerradasPorcentaje = fechastarjetasUnicasRango
+    let arrayCerradasPorcentaje = fechastarjetasUnicasRangoCut
       .sort()
       .map((item, index) => {
         return tarjetas.filter(
@@ -146,7 +166,7 @@ export class GraficoAmarillas extends Component {
     // Datos para el grafico de cerradas porcentaje
 
     const AmarillasAcumuladasAbiertasDataCerradasPorcentaje = [
-      fechastarjetasUnicasRango.map((item, index) => {
+      fechastarjetasUnicasRangoCut.map((item, index) => {
         return {
           x: new Date(
             parseInt(item.slice(0, 4)),
@@ -160,17 +180,33 @@ export class GraficoAmarillas extends Component {
       }),
     ];
 
+    console.log(fechastarjetasUnicasRango);
+    console.log(fechastarjetasUnicasRangoCut);
+
+    const arrayMonths = [];
+
+    for (let i = 1; i < fechastarjetasUnicasRango.length + 1; i++) {
+      arrayMonths.push(i);
+    }
+
+    arrayMonths.reverse();
+
     CanvasJS.addCultureInfo("es", {
       decimalSeparator: ".",
       digitGroupSeparator: ",",
       months: [
-        "domingo",
-        "lunes",
-        "martes",
-        "miércoles",
-        "jueves",
-        "viernes",
-        "sábado",
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre",
       ],
     });
 
@@ -180,7 +216,6 @@ export class GraficoAmarillas extends Component {
       axisX: {
         valueFormatString: "MMMM",
         interval: 1,
-
         intervalType: "month",
       },
       axisY: {
@@ -238,12 +273,45 @@ export class GraficoAmarillas extends Component {
           <Col lg={5} md={12} sm={12}>
             <Card>
               <CardBody>
-                <h3 className="mb-3">Evolucion de Tarjetas amarillas</h3>
+                <div className="d-sm-flex align-items-center">
+                  <div className="">
+                    <div>
+                      <h3 className="mb-3">Evolucion de Tarjetas amarillas</h3>
+                    </div>
+                  </div>
+                  <div className="ml-auto d-sm-flex no-block align-items-center mb-3">
+                    <Col>
+                      <div class="dropdown">
+                        <MoreVertIcon></MoreVertIcon>
+                        <div class="dropdown-content">
+                          {arrayMonths &&
+                            arrayMonths.map((item, index) => {
+                              return <option key={index}>{item}</option>;
+                            })}
+                        </div>
+                      </div>
+                    </Col>
+                  </div>
+                </div>
+
                 <CanvasJSChart
                   culture="en"
                   options={options}
                   onRef={(ref) => (this.chart = ref)}
                 />
+                <Input
+                  type="select"
+                  name="numberMonths"
+                  id="numberMonths"
+                  className="mt-2"
+                  onChange={this.onChange}
+                >
+                  <option>Seleccionar</option>
+                  {arrayMonths &&
+                    arrayMonths.map((item, index) => {
+                      return <option key={index}>{item}</option>;
+                    })}
+                </Input>
               </CardBody>
             </Card>
           </Col>
@@ -255,7 +323,7 @@ export class GraficoAmarillas extends Component {
               tarjetasmesabiertas={array}
               tarjetasmescerradas={arrayCerradas}
               color="Amarillas"
-              fechas={fechastarjetasUnicasRango}
+              fechas={fechastarjetasUnicasRangoCut}
             ></TableModal>
           </Col>
         </Row>
